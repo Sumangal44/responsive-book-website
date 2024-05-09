67a913184fa430f61fe78925f90efad0f619e212
Sumangal44 <123644743+Sumangal44@users.noreply.github.com>
2024-05-09 18:30:24 +0530

    search commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,38 @@
 /*=============== SEARCH ===============*/
-
+const searchButton = document.getElementById("search-button");
+searchClose = document.getElementById("search-close");
+searchContent = document.getElementById("search-content");
+// search show
+// validate if constant exists
+if (searchButton) {
+  searchButton.addEventListener("click", () => {
+    searchContent.classList.add("show-search");
+  });
+}
+// search hidden
+// validate if const is exists
+if (searchClose) {
+  searchClose.addEventListener("click", () => {
+    searchContent.classList.remove("show-search");
+  });
+}
 
 /*=============== LOGIN ===============*/
 
-
 /*=============== ADD SHADOW HEADER ===============*/
-
 
 /*=============== HOME SWIPER ===============*/
 
-
 /*=============== FEATURED SWIPER ===============*/
-
 
 /*=============== NEW SWIPER ===============*/
 
-
 /*=============== TESTIMONIAL SWIPER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
