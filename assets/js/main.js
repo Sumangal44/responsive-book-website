972931bf4b92a55ec47db108626256e7b6a296c5
Sumangal44 <123644743+Sumangal44@users.noreply.github.com>
2024-05-10 09:25:12 +0530

    last commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,6 +37,13 @@ if (loginClose) {
 }
 
 /*=============== ADD SHADOW HEADER ===============*/
+const blurHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
+};
+window.addEventListener("scroll", blurHeader);
 
 /*=============== HOME SWIPER ===============*/
 let swiperHome = new Swiper(".home__swiper", {
@@ -55,17 +62,125 @@ let swiperHome = new Swiper(".home__swiper", {
     },
   },
 });
-
 /*=============== FEATURED SWIPER ===============*/
+let swiperFeatured = new Swiper(".featured__swiper", {
+  loop: true,
+  spaceBetween: 16,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false,
+  // },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1150: {
+      slidesPerView: 3,
+      centeredSlides: false,
+    },
+  },
+});
 
 /*=============== NEW SWIPER ===============*/
+let swiperNew = new Swiper(".new__swiper", {
+  loop: true,
+  spaceBetween: 16,
+  slidesPerView: "auto",
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false,
+  // },
+  breakpoints: {
+    1150: {
+      slidesPerView: 3,
+    },
+  },
+});
 
 /*=============== TESTIMONIAL SWIPER ===============*/
+let swiperTestmonial = new Swiper(".testimonial__swiper", {
+  loop: true,
+  spaceBetween: 16,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+  breakpoints: {
+    1150: {
+      slidesPerView: 3,
+      centeredSlides: false,
+    },
+  },
+});
 
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
 
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 /*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
+}
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  //reset:true
+});
+sr.reveal(`.home__data, .featured__container ,.new__container, .join__data,
+.testimonial__container,.footer`);
+sr.reveal(`.home__images`, { delay: 600 });
+sr.reveal(`.services__card`, { interval: 100 });
+sr.reveal(`.discount__data`, { origin: `left` });
+sr.reveal(`.discount__images`, { origin: `right` });
